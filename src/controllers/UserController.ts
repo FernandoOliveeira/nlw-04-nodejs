6112a6f0bf637a5383380c9a00b6cce74075743f
Fernando <fer.oliveeira@hotmail.com>
2021-02-26 13:49:24 -0300

    feat: added yup for data validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, RepositoryNotTreeError } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from 'yup';
+
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const {name, email} = request.body;
-        
-        const usersRepository = getCustomRepository(UsersRepository);
 
+        // Validação dos campos nome e email
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required(),            
+        })
+
+        try{
+            await schema.validate(request.body, {abortEarly: false})
+        }catch(err){
+            return response.status(400).json({error: err})
+        }
+        
+
+
+        const usersRepository = getCustomRepository(UsersRepository);
+        // SELECT * FROM USERS WHERE EMAIL = EMAIL
         const userAlreadyExists = await usersRepository.findOne({
             email
         });
@@ -23,6 +39,7 @@ class UserController {
             name, email
         });
 
+        // Salva o usuário no banco de dados
         await usersRepository.save(user);
 
         return response.status(201).json(user);
